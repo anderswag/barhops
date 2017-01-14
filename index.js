e1a4cba6bd05ff6341f3437c8c7558f9932de8e6
anderswag <anderson.hung@live.com>
2017-01-14 15:11:28 -0500

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express')
+const app = express()
+const bodyParser  = require("body-parser");
+
+// Routes
+const getNearBars        = require("./routes/getNearBars");
+
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.use("/api/getNearBars", getNearBars());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
---
+++ routes/getNearBars.js
@@ -0,0 +1,34 @@
+"use strict";
+const request = require('request');
+const express = require('express');
+const router  = express.Router();
+
+
+function getTheStuff(queryParams){
+
+  let data = {
+    location: '-33.8670522,151.1957362',
+    radius:'500',
+    type:'bar'
+  }
+
+  let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${data.location}&radius=${data.radius}&type=${data.type}&key=AIzaSyCYzKfH2z2nEiecJN5m0fhiEGayUxnKFMM`;
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+    }
+  })
+
+}
+
+
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res) => {
+    res.json({'asdf':'1234'});
+  });
+
+  return router;
+}
